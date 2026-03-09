0168e26e2f86e3e681a8680128a2d162614b2cdc
ezung123 <zarenz18@gmail.com>
2026-03-09 18:01:34 +0530

    Added customer risk labeling and recommendations

--- pages/3_Data_explorer.py
+++ pages/3_Data_explorer.py
@@ -11,3 +11,16 @@ st.dataframe(df)
 fig = px.histogram(df, x="Contract", color="Churn")
 
 st.plotly_chart(fig)
+
+fig = px.pie(df, names="Churn")
+st.plotly_chart(fig)
+
+fig = px.histogram(df, x="Contract", color="Churn")
+st.plotly_chart(fig)
+
+fig = px.box(df, x="Churn", y="MonthlyCharges")
+st.plotly_chart(fig)
+
+fig = px.box(df, x="Churn", y="tenure")
+st.plotly_chart(fig)
+
